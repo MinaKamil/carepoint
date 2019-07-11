8806055e2ada4dbbd3f3d60a9e102c3d6a31f767
mbottros <minakamil52@gmail.com>
2019-07-11 18:44:15 +0200

    add scroll top button

---
+++ dist/js/main.min.js
@@ -0,0 +1,12 @@
+var btnScroll = document.getElementById('btn-scroll');
+window.onscroll = function () {
+  if (window.pageYOffset >= 20) {
+    btnScroll.style.display = 'block';
+  } else {
+    btnScroll.style.display = 'none';
+  }
+
+};
+btnScroll.onclick = function () {
+  window.scrollTo(0, 0);
+};
